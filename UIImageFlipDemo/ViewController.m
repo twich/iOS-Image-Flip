f60dec24c928cfe5493c327d383fdd5c4f33ea69
twich <scott.twichel@gmail.com>
2014-07-30 23:09:34 -0600

    Added a variable to track which image is visible, removed button, added touchesBegan method to trigger flip animation

--- UIImageFlipDemo/ViewController.m
+++ UIImageFlipDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    BOOL secondImageVisible;
+}
 
 @end
 
@@ -18,6 +20,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    secondImageVisible = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,14 +28,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)flipImageButton:(id)sender {
+- (void)flipImage{
+    NSString *imageName;
+    if (secondImageVisible) {
+        imageName = @"ld_home_home_title.png";
+        secondImageVisible = NO;
+    }else{
+        imageName = @"ld_home_donor_title.png";
+        
+        secondImageVisible = YES;
+    }
     
-    UIImage *secondImage = [UIImage imageNamed:@"ld_home_donor_title.png"];
+    UIImage *secondImage = [UIImage imageNamed:imageName];
     [UIView transitionWithView:_titleImage
                       duration:1
                        options:UIViewAnimationOptionTransitionFlipFromRight
                     animations:^{_titleImage.image = secondImage;}
                     completion:nil];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self flipImage];
 }
 
 @end
